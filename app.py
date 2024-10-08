5a79361df1440f444d3c5e8d8ffaea78b3d87c3f
lucasguerra12 <lucasguerrawr2004@gmail.com>
2024-10-08 17:36:37 -0300

    flask arrumado e primeira rota feita

--- app.py
+++ app.py
@@ -1,15 +1,25 @@
-from flask import Flask, render_template
-
-app = Flask(__name__)
 
 
-@app.route('/pagina-especifica')
+@app.route('/paginahome')
 def pagina_especifica():
-    return render_template('pagina_especifica.html')
+    return render_template('baseflask.html')
 
-@app.route('/')
-def home():
-    return render_template('base.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+
+
+
+
+
+
+@app.route('/paginahome')
+def pagina_especifica():
+    return render_template('baseflask.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
