f74e15bc69e5e23fd5c95335ea93aa9a10addbb6
lucasguerra12 <lucasguerrawr2004@gmail.com>
2024-10-08 17:37:10 -0300

    rota criada

--- app.py
+++ app.py
@@ -1,25 +1,18 @@
+from flask import Flask, render_template
 
+app = Flask(__name__)
 
-@app.route('/paginahome')
-def pagina_especifica():
-    return render_template('baseflask.html')
+@app.route('/')
+def home():
+    return render_template('home.html')
 
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
-
-
-
-
-
-
-
+@app.route('/sobre')
+def sobre():
+    return render_template('sobre.html')
 
 @app.route('/paginahome')
 def pagina_especifica():
-    return render_template('baseflask.html')
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
